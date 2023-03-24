4cd1c8edc8f8012d00d126ff3cbe2f384f9b554b
Davi <davisiqueira591@gmail.com>
2023-03-23 21:05:43 -0300

    fiz o bagulho caralho

--- src/components/product/index.tsx
+++ src/components/product/index.tsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './styles.css'
 
 const Product = ({productName, value, quantity}: {productName: string, value: number, quantity: number}) => {
+  const [quantityChanger, setQuantityChanger] = useState(quantity)
+
+  const handleSubtractQuantity = () => {
+    if(quantityChanger>0) {
+      let quantity = quantityChanger-1
+      setQuantityChanger(quantity)
+    }
+  }
+
+  const handleAddQuantity = () => {
+    let quantity = quantityChanger+1
+    setQuantityChanger(quantity)
+  }
 
   return (
     <div className="product">
       <p className="client-name">Name: {productName}</p>
       <p className="value">{value.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</p>
       <div className="quantity-wrapper">
-        <button>-</button>
-        <p>{quantity}</p>
-        <button>+</button>
+        <button onClick={handleSubtractQuantity}>-</button>
+        <p>{quantityChanger}</p>
+        <button onClick={handleAddQuantity}>+</button>
       </div>
     </div>
   )
